fdf8d3bec89c6460176c755bb655ab2a3474860a
Denis Goida <www.vdcdj@gmail.com>
2023-03-29 21:18:22 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HeaderMenuComponent } from './header/header-menu/header-menu.component';
-import { HeaderNavbarComponent } from './header/header-navbar/header-navbar.component';
-import { ImagesComponent } from './components/images/images.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HeaderMenuComponent,
-    HeaderNavbarComponent,
-    ImagesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
